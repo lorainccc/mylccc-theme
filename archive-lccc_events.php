3e20b4c1c47a92ff3d046802cb23559ad4efd2fc
David Michael Brattoli <dbrattoli@lorainccc.edu>
2016-12-12 14:01:08 -0500

    replaced the archive title

--- archive-lccc_events.php
+++ archive-lccc_events.php
@@ -16,8 +16,8 @@ get_header(); ?>
 		if ( have_posts() ) : ?>
 
 			<header class="page-header">
+				<h1 class="page-title"> Events</h1>
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
